5e6bb537e7be82b81f08ecfb8f59c2cad73e1897
sean <sean@sean-PC>
2016-06-24 14:52:11 -0400

    File /C:/Users/sean/OneDrive/GISProject/src/DataParser.java changed at Fri Jun 24 14:52:09 EDT 2016

--- src/DataParser.java
+++ src/DataParser.java
@@ -74,8 +74,7 @@ public class DataParser {
 			//System.out.println((str).getBytes());
 			dataFile.write((str +"\n").getBytes());
 		}
-		//dataFile.seek(265); // reset the pointer in the file to 0;
-		dataFile.seek(0);
+		dataFile.seek(265); // reset the pointer in the file to 0;
 		gisRecord.close();
 		stat.write("\n");
 		
