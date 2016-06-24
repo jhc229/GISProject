590f42392a04739587a4c8d76713ef230f5751d5
sean <sean@sean-PC>
2016-06-24 11:12:29 -0400

    File /C:/Users/sean/OneDrive/GISProject/src/HashTable.java changed at Fri Jun 24 11:12:27 EDT 2016

--- src/HashTable.java
+++ src/HashTable.java
@@ -67,8 +67,8 @@ public class HashTable<Key, E> {
 		int home; 
 		int count = 0; //update probe
 		int pos = home = Math.abs(e.hashCode() % table.length); 
-		//for (int i =1; tableList[pos] != null; i++){ // check duplicates?
-		for (int i = 0; i < table.length; i++){
+		for (int i =1; tableList[pos] != null; i++){ // check duplicates?
+		//for (int i = 0; i < table.length; i++){
 			if (table[pos] == null){
 				table[pos] = new KVpair<Key, E>(k, e);
 				break;
