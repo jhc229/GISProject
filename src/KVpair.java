59c111f8752dd0dd44515d0c9d304f98c9256f56
sean <sean@sean-PC>
2016-06-24 03:04:58 -0400

    File /C:/Users/sean/OneDrive/GISProject/src/KVpair.java changed at Fri Jun 24 03:04:57 EDT 2016

---
+++ src/KVpair.java
@@ -0,0 +1,21 @@
+
+
+class KVpair {
+
+	private Key k;
+	private E e;
+	
+	// Constructor
+	KVpair(){
+		k = null;
+		e = null;
+		
+	}
+	
+	KVpair(Key k, E e){
+		this.k = k;
+		this.e =e	
+	}
+	
+	
+}
