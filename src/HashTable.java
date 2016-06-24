09c2b83df50afd078dc9325d0c600cf0140eef49
sean <sean@sean-PC>
2016-06-24 03:14:26 -0400

    File /C:/Users/sean/OneDrive/GISProject/src/HashTable.java changed at Fri Jun 24 03:14:24 EDT 2016

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,5 +1,10 @@
 /**
- * 
+ * Your table will use quadratic probing to resolve collisions, with the quadratic function (n^2+ n)/2 to compute the step size. 
+ * The hash table must use a contiguous physical structure (array). The initial size of the table will be 1024, and the table will
+ * resize itself automatically, by doubling its size whenever the table becomes 70% full.
+ * Since the specified table sizes given above are powers of 2, an empty slot will always be found unless the table is full.
+ * You will use the elfhash() function from the course notes, and apply it to the concatenation of the feature name and state
+ * (abbreviation) field of the data records. Precisely how you form the concatenation is up to you.
  * @author sean
  *
  * @param <Key>
@@ -7,5 +12,13 @@
  */
 public class HashTable<Key, V> {
 	
+	private KVpair<Key, V>[] tableList;
+	private int longestProbe = 0;
+	
+	
+	// Constructor
+	public HashTable(int size){
+		tableList = 
+	}
 
 }
