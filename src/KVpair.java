82d19452d579b1c249d095ecf1a5758cb2900a89
sean <sean@sean-PC>
2016-06-24 11:52:43 -0400

    File /C:/Users/sean/OneDrive/GISProject/src/KVpair.java changed at Fri Jun 24 11:52:42 EDT 2016

--- src/KVpair.java
+++ src/KVpair.java
@@ -33,7 +33,7 @@ class KVpair<Key, E> {
 	
 	public String kvPairtoString(){
 		String output = "";
-		return output = getKey().toString() + ", " + getValue();
+		return output = getKey() + ", " + getValue();
 	}
 	
 }
