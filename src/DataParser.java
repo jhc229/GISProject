8c5a99d8ec5548de52fe57a9e99224573c9bf6e9
sean <sean@sean-PC>
2016-06-24 10:16:11 -0400

    File /C:/Users/sean/OneDrive/GISProject/src/Point.java changed at Fri Jun 24 10:16:10 EDT 2016

--- src/DataParser.java
+++ src/DataParser.java
@@ -71,7 +71,7 @@ public class DataParser {
 				gisRecord.readLine(); //Moves the pointer to next line
 			}
 			endOffset +=str.length() +1;
-			System.out.println((str).getBytes());
+			//System.out.println((str).getBytes());
 			dataFile.write((str +"\n").getBytes());
 		}
 		dataFile.seek(265); // reset the pointer in the file to 0;
@@ -83,10 +83,10 @@ public class DataParser {
 	//One degree is equal to 60 minutes and equal to 3600 seconds:
 	public void world(String westLong, String eastLong, String southLat, String northLat) throws IOException {
 				
-		wLong = toCoord(Integer.parseInt(westLong.substring(0, 2)), Integer.parseInt(westLong.substring(2, 4)), Integer.parseInt(westLong.substring(4, 6)), westLong.substring(6)).toSeconds();
-		eLong = toCoord(Integer.parseInt(eastLong.substring(0, 2)), Integer.parseInt(eastLong.substring(2, 4)), Integer.parseInt(eastLong.substring(4, 6)), eastLong.substring(6)).toSeconds();
-		sLat =  toCoord(Integer.parseInt(southLat.substring(0, 3)), Integer.parseInt(southLat.substring(3, 5)), Integer.parseInt(southLat.substring(5, 7)), southLat.substring(7)).toSeconds();
-		nLat = toCoord(Integer.parseInt(northLat.substring(0, 3)), Integer.parseInt(northLat.substring(3, 5)), Integer.parseInt(northLat.substring(5, 7)), northLat.substring(7)).toSeconds();
+		wLong = toCoord(Integer.parseInt(westLong.substring(0, 3)), Integer.parseInt(westLong.substring(3, 5)), Integer.parseInt(westLong.substring(5, 7)), westLong.substring(7)).toSeconds();
+		eLong = toCoord(Integer.parseInt(eastLong.substring(0, 3)), Integer.parseInt(eastLong.substring(3, 5)), Integer.parseInt(eastLong.substring(5, 7)), eastLong.substring(7)).toSeconds();
+		sLat =  toCoord(Integer.parseInt(southLat.substring(0, 2)), Integer.parseInt(southLat.substring(2, 4)), Integer.parseInt(southLat.substring(4, 6)), southLat.substring(6)).toSeconds();
+		nLat = toCoord(Integer.parseInt(northLat.substring(0, 2)), Integer.parseInt(northLat.substring(2, 4)), Integer.parseInt(northLat.substring(4, 6)), northLat.substring(6)).toSeconds();
 
 	//	quadTree = new prQuadTree<Point>(wLong, eLong , sLat , nLat);
 		stat.write(wLong+ " "+ eLong +" "+ sLat +" "+ nLat);
@@ -121,9 +121,12 @@ public class DataParser {
 	//		System.out.println("offset: " + offset);
 			
 			gisRecords.gisRecordsUpdate(offset);
-			System.out.println(GeoFeatures.FEATURE_NAME);
+			//System.out.println(GeoFeatures.FEATURE_NAME);
 			NameIndex names = new NameIndex(GeoFeatures.FEATURE_NAME, GeoFeatures.STATE_ALPHA);
 			Point pos = new Point(GeoFeatures.PRIM_LONG_DMS.toSeconds(), GeoFeatures.PRIMARY_LAT_DMS.toSeconds(), (int) offset);
+			table.insertHash(names, (int) offset);
+			//System.out.println("Number of elements: " + table.getNumElements());
+		//	System.out.println("Number of probes: " + table.getProbe());
 			
 			if (pos.inBox(wLong, eLong , sLat , nLat)){
 				table.insertHash(names, (int) offset);
