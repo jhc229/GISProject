273554ba1b04ba291e8a1c7fbcaedda17f5db5ef
sean <sean@sean-PC>
2016-06-24 22:13:36 -0400

    File /C:/Users/sean/OneDrive/GISProject/src/GISRecordParser.java changed at Fri Jun 24 22:13:34 EDT 2016

--- src/GISRecordParser.java
+++ src/GISRecordParser.java
@@ -75,7 +75,7 @@ public class GISRecordParser {
 			GeoFeatures.SOURCE_LONG_DEC =0;
 			
 			if (!items[15].equals("")) GeoFeatures.ELEV_IN_M = Integer.parseInt(items[15]);
-			GeoFeatures.ELEV_IN_FT = Integer.parseInt(items[16]);
+			if (!items[16].equals("")) GeoFeatures.ELEV_IN_FT = Integer.parseInt(items[16]);
 			GeoFeatures.MAP_NAME = items[17];
 			GeoFeatures.DATE_CREATED = items[18];
 			
