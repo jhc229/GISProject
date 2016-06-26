4863514ea250dea85923fc32175a88830607352a
sean <sean@sean-PC>
2016-06-26 13:36:48 -0400

    File /C:/Users/sean/OneDrive/GISProject/src/quadTree/Point.java changed at Sun Jun 26 13:36:45 EDT 2016

--- src/quadTree/Point.java
+++ src/quadTree/Point.java
@@ -1,93 +1,94 @@
 package quadTree;
+
 import java.util.Vector;
 
 /**
- * Store a geographic coordinate and a collection of the file offsets of the matching
- * GIS records in the database file.
+ * Store a geographic coordinate and a collection of the file offsets of the
+ * matching GIS records in the database file.
  * 
  * @author sean
  *
  */
 public class Point implements Compare2D<Point> {
 
-	private long  					xcoord;
-	private long						ycoord;
-	private Vector<Long>		 offsets;
-	
-	public Point(){
+	private long xcoord;
+	private long ycoord;
+	private Vector<Long> offsets;
+
+	public Point() {
 		xcoord = 0;
 		ycoord = 0;
 	}
-	
-	public Point(int x, int y, long z){
+
+	public Point(int x, int y, long z) {
 		xcoord = x;
 		ycoord = y;
 		offsets = new Vector<Long>();
 		offsets.add(z);
 	}
-	
-	public long getX(){
+
+	public long getX() {
 		return xcoord;
 	}
 
-	public long getY(){
+	public long getY() {
 		return ycoord;
 	}
-	
-	public void addOffset(Vector<Long> offset){
+
+	public void addOffset(Vector<Long> offset) {
 		this.offsets.addAll(offset);
 	}
-	
-	public Vector<Long> getOffset(){
-		 return offsets;
+
+	public Vector<Long> getOffset() {
+		return offsets;
 	}
-	   
-	 public Direction directionFrom(long X, long Y) { 
-			if ( xcoord <= X && ycoord > Y) return Direction.NW;
 
-			else if(xcoord > X && ycoord >= Y) return Direction.NE;
-			
-			else if(ycoord < Y && xcoord >= X) return Direction.SE;
+	public Direction directionFrom(long X, long Y) {
+		if (xcoord <= X && ycoord > Y) return Direction.NW;
 
-			else if(xcoord < X && ycoord <= Y) return Direction.SW;
-			
-			return Direction.NOQUADRANT;
-	   }
-	   
-	   public Direction inQuadrant(double xLo, double xHi,  double yLo, double yHi) { 
-			if ( !inBox(xLo, xHi, yLo, yHi)) return Direction.NOQUADRANT;
-			else{
-				Direction dir = directionFrom((long) (xLo + xHi)/2, (long) (yLo + yHi)/2);
-				if(dir == Direction.NOQUADRANT) return Direction.NE;
-				return dir;
-			}
-	   }
+		else if (xcoord > X && ycoord >= Y) return Direction.NE;
 
-	   public boolean inBox(double xLo, double xHi, double yLo, double yHi) { 
-			/*if ( xcoord < xLo || xcoord > xHi || ycoord < yLo || ycoord > yHi){
-				return false;
-			}
-			return true;*/
-			return xcoord >= xLo && xcoord <= xHi && ycoord >= yLo && ycoord <= yHi;
-	   }
+		else if (xcoord < X && ycoord <= Y) return Direction.SW;
+		
+		else if (xcoord >= X && ycoord < Y ) return Direction.SE;
 
-	   public boolean equals(Object o) { 
-			if ( o.getClass().equals(this.getClass())){
-	    		return this.getClass().cast(o).directionFrom(xcoord, ycoord) == Direction.NOQUADRANT;
-	    	}
-			return false;
-			/*
-			 * 	if ( !o.getClass().equals(this.getClass())) return false;
-			 * if (this.getClass().cast(o).directionFrom(xcoord, ycoord) == Direction.NOQUADRANT){
-				return true;
-			}
-			return false;*/
 
-	   }
-	   
-	   public String toString() {
-		   return "[( "+ xcoord+ ", " + ycoord + ")," + offsets.toString() + "]";
-	   }
-	   
+		return Direction.NOQUADRANT;
+	}
+
+	public Direction inQuadrant(double xLo, double xHi, double yLo, double yHi) {
+		if (!inBox(xLo, xHi, yLo, yHi)) return Direction.NOQUADRANT;
+		else {
+			Direction dir = directionFrom((long) (xLo + xHi) / 2, (long) (yLo + yHi) / 2);
+			if (dir == Direction.NOQUADRANT) return Direction.NE;
+			return dir;
+		}
+	}
+
+	public boolean inBox(double xLo, double xHi, double yLo, double yHi) {
+		/*
+		 * if ( xcoord < xLo || xcoord > xHi || ycoord < yLo || ycoord > yHi){
+		 * return false; } return true;
+		 */
+		return xcoord >= xLo && xcoord <= xHi && ycoord >= yLo && ycoord <= yHi;
+	}
+
+	public boolean equals(Object o) {
+		if (o.getClass().equals(this.getClass())) {
+			return this.getClass().cast(o).directionFrom(xcoord,
+					ycoord) == Direction.NOQUADRANT;
+		}
+		return false;
+		/*
+		 * if ( !o.getClass().equals(this.getClass())) return false; if
+		 * (this.getClass().cast(o).directionFrom(xcoord, ycoord) ==
+		 * Direction.NOQUADRANT){ return true; } return false;
+		 */
 
 	}
+
+	public String toString() {
+		return "[( " + xcoord + ", " + ycoord + ")," + offsets.toString() + "]";
+	}
+
+}
