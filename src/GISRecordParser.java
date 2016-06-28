66aa26b2323fc498054f1307435e694a18ec5a23
sean <sean@sean-PC>
2016-06-28 16:03:41 -0400

    File /C:/Users/sean/OneDrive/GISProject/src/GISRecordParser.java changed at Tue Jun 28 16:03:40 EDT 2016

--- src/GISRecordParser.java
+++ src/GISRecordParser.java
@@ -49,6 +49,7 @@ public class GISRecordParser {
 			dataRec.LINE = line;
 			//System.out.println("ID:  " + GeoFeatures.FEATURE_ID );
 			System.out.println("Offset " +items[0] );
+			System.out.println("Offset " +items[0] );
 			dataRec.FEATURE_ID = Integer.parseInt(items[0]);
 			dataRec.FEATURE_NAME = items[1];
 			dataRec.FEATURE_CLASS = items[2];
