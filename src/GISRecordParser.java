86f4231374929cdda442ba9cb5351d9588f24f40
sean <sean@sean-PC>
2016-06-28 15:31:06 -0400

    File /C:/Users/sean/OneDrive/GISProject/src/GISRecordParser.java changed at Tue Jun 28 15:31:05 EDT 2016

--- src/GISRecordParser.java
+++ src/GISRecordParser.java
@@ -121,7 +121,7 @@ public class GISRecordParser {
 	
 	public GeoFeatures gisUpdate(String line){
 		
-		System.out.println(line);
+		System.out.println("asdasdasasdasd:"ine);
 		return null;
 		
 	}
