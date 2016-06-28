198d4f849313e7381c900af52ba604985302a7d0
sean <sean@sean-PC>
2016-06-28 05:31:44 -0400

    File /C:/Users/sean/OneDrive/GISProject/src/quadTree/prQuadTree.java changed at Tue Jun 28 05:31:43 EDT 2016

--- src/quadTree/prQuadTree.java
+++ src/quadTree/prQuadTree.java
@@ -395,7 +395,7 @@ private T fFind(prQuadNode rt, T elem, double xMin, double xMax,
 			for (int i = 0; i < leafNode.Elements.size(); i++) {
 
 				// check if elem is within the region wanted
-				if (leafNode.Elements.get(i).inBox(x1, x2, y1, y2) {
+				if (leafNode.Elements.get(i).inBox(x1, x2, y1, y2) ){
 					res.add(leafNode.Elements.get(i));
 				}
 			}
