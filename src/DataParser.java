260aa0387d3d68ca19bd1d9e9b0e81f16860fbd6
sean <sean@sean-PC>
2016-06-28 17:39:29 -0400

    File /C:/Users/sean/OneDrive/GISProject/src/DataParser.java changed at Tue Jun 28 17:39:28 EDT 2016

--- src/DataParser.java
+++ src/DataParser.java
@@ -506,7 +506,8 @@ public class DataParser {
 				
 				//str +=dataRec.OFFSET +  ":	" + dataRec.FEATURE_NAME + " " + dataRec.COUNTY_NAME + " "+dataRec.STATE_ALPHA + "\n";
 				//pool.add(dataRec.OFFSET, dataRec.FEATURE_NAME, dataRec.COUNTY_NAME, dataRec.STATE_ALPHA);
-			
+				dataFile.seek(265);
+
 			}
 		}
 		return temp;
