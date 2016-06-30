ede3bd5b0e875166ee8189b23a93f031d558a014
sean <sean@sean-PC>
2016-06-30 15:32:17 -0400

    File /C:/Users/sean/OneDrive/GISProject/src/GISRecordParser.java changed at Thu Jun 30 15:32:15 EDT 2016

--- src/GISRecordParser.java
+++ src/GISRecordParser.java
@@ -57,8 +57,8 @@ public class GISRecordParser {
 			dataRec.FEATURE_ID = Integer.parseInt(items[0]);
 			dataRec.FEATURE_NAME = items[1];
 			dataRec.FEATURE_CLASS = items[2];
-			dataRec.STATE_ALPHA= items[3];
-			dataRec.STATE_NUMERIC = Double.parseDouble(items[4]);
+			if (!items[6].equals("")){
+				dataRec.COUNTY_NUMERIC = Integer.parseInt(items[6]);
 			dataRec.COUNTY_NAME = items[5];
 			dataRec.COUNTY_NUMERIC = Double.parseDouble(items[6]);
 			if( Double.parseDouble(items[9]) == 0){
@@ -115,7 +115,7 @@ public class GISRecordParser {
 		dataRec.FEATURE_NAME = items[1];
 		dataRec.FEATURE_CLASS = items[2];
 		dataRec.STATE_ALPHA= items[3];
-		dataRec.STATE_NUMERIC = Double.parseDouble(items[4]);
+		dataRec.STATE_NUMERIC = Integer.parseInt(items[4]);
 		dataRec.COUNTY_NAME = items[5];
 		if (!items[6].equals("")){
 			dataRec.COUNTY_NUMERIC = Integer.parseInt(items[6]);
